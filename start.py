836bcd15222bc2592b4d9f03714e6f59b3b74a8f
Lian Mo <molian@dc3wap07.dc.umich.edu>
2017-03-05 17:45:26 -0500

    ports map

--- start.py
+++ start.py
@@ -12,17 +12,27 @@ DEFAULT_NUM_CLIENTS = 2
 DEFAULT_PORT_NUM = 6000
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
+def create_ports_map(n, ip_add, ports):
+	s = {}
+	for i in range(0, n):
+		s[i] = [ip_add+str(i), ports+i]
+	return s
+
 def Paxoservice():
+
+	server_ports_info = create_ports_map(2*DEFAULT_NUM_FAILURES+1, "127.0.0.", 5000)
+	client_ports_info = create_ports_map(DEFAULT_NUM_CLIENTS, "128.0.0.", 5000)
+
     # create replicas
     client_list = [] #[process , event]
     for i in range(DEFAULT_NUM_CLIENTS):
     	e = multiprocessing.Event()
-    	p = multiprocessing.Process(target = client.Client , args = (None , None, i , None , e))
+    	p = multiprocessing.Process(target = client.Client , args = (client_ports_info[i][0] , client_ports_info[i][1], i , server_ports_info , e))
     	p.start()
     	client_list.append([p , e])
 
     for i in range(2*DEFAULT_NUM_FAILURES+1):
-        p = multiprocessing.Process(target=replica.Replica, args = (DEFAULT_NUM_FAILURES , i , None , None)) #f, ID, port_info
+        p = multiprocessing.Process(target=replica.Replica, args = (DEFAULT_NUM_FAILURES , i , server_ports_info , client_ports_info)) #f, ID, port_info
         p.start()
     # operation here
 
